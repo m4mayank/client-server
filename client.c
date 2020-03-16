2aeea3736f735336a7e656ebb8adaa3ad4a3772d
Mayank Marwaha <bunnymarwaha.bm@gmail.com>
2020-03-16 16:11:14 -0600

    basic implementation and syntax check

--- client.c
+++ client.c
@@ -1,1 +1,42 @@
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
+int main(){
+  //create a socket
+  int network_socket;
+  network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  //specify an address for the socket
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(9002);
+  server_address.sin_addr.s_addr = htons(INADDR_ANY);
+
+  int connection_status = connect(network_socket, (struct sockaddr *) &server_address,sizeof(server_address));
+  //check for error with connection
+  if (connection_status < 0){
+    printf("Error connecting to the remote socket");
+  }
+
+  //sending the initial message to server
+  char msg[20] = "Hello Server";
+  send(network_socket, msg, sizeof(msg),0);
+
+  //receive data from the socket
+  char server_response[256];
+  recv(network_socket, &server_response, sizeof(server_response),0);
+
+  //print out the server's response
+  printf("The server sent the data : %s\n", server_response);
+
+  //and then close the socket
+  close(network_socket);
+  return 0;
+}
--- server.c
+++ server.c
@@ -1,79 +1,44 @@
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <errno.h>
-#include <netdb.h>
+#include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/sysctl.h>
-#include "funct.c"
-/*
-#define MAXLINE 4096
-#define bzero(ptr,n) memset(ptr, 0, n)
-*/
-/* include Socket */
-/*
-int
-Socket(int family, int type, int protocol)
-{
-  int n;
-  if((n=socket(family, type, protocol)) < 0)
-    err_sys("socket error");
-  return(n);
-}
-*/
-/* end Socket */
-/*
-void Bind(int fd, const struct sockaddr *sa, socklen_t salen){
-  if (bind(fd, sa, salen) < 0)
-    err_sys("bind error");
-}
-*/
-/* include Listen */
-/*
-void
-Listen(int fd, int backlog)
-{
-    char  *ptr;
-    if((ptr=getenv("LISTENQ"))!=NULL)
-      backlog=atoi(ptr);
-    if(listen(fd, backlog)<0)
-      err_sys("listen error");
-}
-*/
-/* end Listen */
-/*
-int Accept(int fd, struct sockaddr *sa, socklen_t *salenptr){
-  int n;
-again:
-  if ( (n = accept(fd, sa, salenptr)) < 0) {
-#ifdef  EPROTO
-  if (errno == EPROTO || errno == ECONNABORTED)
-#else
-  if (errno == ECONNABORTED)
-#endif
-    goto again;
-  else
-    err_sys("accept error");
-  }
-  return(n);
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
-}
-*/
 
-int main(int argc, char **argv){
-  int listenfd, connfd,n;
-  struct sockaddr_in servaddr;
-  char buff[MAXLINE];
-  char recvline[MAXLINE + 1];
-  struct message msg;
-  listenfd = Socket(AF_INET, SOCK_STREAM, 0);
-  bzero(&servaddr, sizeof(servaddr));
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(0);
-  Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
-  Listen(listenfd, LISTENQ);
-  for(;;){
-    connfd = Accept(listenfd, (SA *) NULL, NULL);
-  }
+int main(){
+  char server_message[256]="You have connected to the server!";
+
+  //create a socket
+  int server_socket;
+  server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+  //specify an address for the socket
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(9002);
+  server_address.sin_addr.s_addr = htons(INADDR_ANY);
+
+  //Bind the socket to our specified IP and Port
+  bind(server_socket, (struct sockaddr *) &server_address,sizeof(server_address));
+
+  listen(server_socket, 4);
+
+  int client_socket;
+  client_socket=accept(server_socket,(struct sockaddr *)NULL, NULL);
+
+  //receive the message after accepting the connection
+  char client_response[20];
+  recv(client_socket, &client_response, sizeof(client_response),0);
+  printf("client sent : %s\n", client_response);
+
+  //send the message
+  send(client_socket, server_message, sizeof(server_message),0);
+
+  //close the socket
+  close(server_socket);
+  return 0;
 }
